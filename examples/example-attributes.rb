9ebd43bc78fa75793e3961e9b79e01c752cf6078
Sean O'Halpin <sean.ohalpin@gmail.com>
2008-12-03 01:04:16 +0000

    added more example attributes

--- examples/example-attributes.rb
+++ examples/example-attributes.rb
@@ -2,10 +2,32 @@ require 'ffi-ncurses'
 include NCurses
 initscr
 begin
-  attr_set A_BOLD, 0, nil
-  addstr "THIS IS BOLD"
-  attr_set A_NORMAL, 0, nil
-  addstr "THIS IS NORMAL"
+  attributes = %w[
+    A_BLINK     
+    A_BOLD      
+    A_DIM
+    A_NORMAL
+    A_REVERSE   
+    A_STANDOUT  
+    A_UNDERLINE 
+  ]
+  alt_attributes = %w[
+    A_ALTCHARSET
+    A_HORIZONTAL
+    A_INVIS     
+    A_LEFT      
+    A_LOW       
+    A_PROTECT     
+    A_RIGHT     
+    A_TOP       
+    A_VERTICAL
+  ]
+
+  attributes.each do |attr|
+    attr_const = NCurses.const_get(attr)
+    attr_set attr_const, 0, nil
+    addstr "THIS IS #{attr}\n"
+  end
 
   refresh
   ch = getch
