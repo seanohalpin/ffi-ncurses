aab7f15a8dfb094c8a9cb726d3476ddda7273ba3
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-02-16 00:31:35 +0000

    tweaked docs

---
+++ examples/doc-eg3.rb
@@ -0,0 +1,19 @@
+require 'ffi-ncurses'
+include FFI::NCurses
+begin
+  initscr
+  win = newwin(6, 12, 15, 15)
+  box(win, 0, 0)
+  inner_win = newwin(4, 10, 16, 16)
+  waddstr(inner_win, (["Hello!"] * 5).join(' '))
+  wrefresh(win)
+  wrefresh(inner_win)
+  ch = wgetch(inner_win)
+  delwin(win)
+
+rescue Object => e
+  FFI::NCurses.endwin
+  puts e
+ensure
+  FFI::NCurses.endwin
+end
