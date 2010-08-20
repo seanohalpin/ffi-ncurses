3c120415bca9fa03044943f7fe417fa7eab8828b
Sean O'Halpin <sean.ohalpin@bbc.co.uk>
2010-08-20 12:10:19 +0100

    use Ctrl-Q to quit key example

--- examples/example-keys.rb
+++ examples/example-keys.rb
@@ -4,17 +4,19 @@
 #
 require 'ffi-ncurses'
 include FFI::NCurses
+
 begin
   initscr
   raw
   keypad stdscr, 1
   noecho
   curs_set 0
+
   ch = 0
   name = "none"
-  while ch != 27
+  while ch != KEY_CTRL_Q
     clear
-    addstr "Press any key (Escape to exit): "
+    addstr "Press any key (Ctrl Q to quit): "
     printw "name: %s dec: %d char: [%c]", :string, name, :int, ch, :int, ch
     refresh
     ch = getch
