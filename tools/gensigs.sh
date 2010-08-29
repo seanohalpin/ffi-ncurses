af6f66b0c3305e6d7366bac2919a0f26e82fe1f3
Sean O'Halpin <sean.ohalpin@gmail.com>
2010-08-29 05:00:52 +0100

    use ncursesw as base

--- tools/gensigs.sh
+++ tools/gensigs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# INPUT_FILE=/usr/include/ncursesw/ncurses.h
-INPUT_FILE=ncurses.h
+INPUT_FILE=/usr/include/ncursesw/ncurses.h
+# INPUT_FILE=ncurses.h
+# INPUT_FILE=/usr/include/ncurses.h
 # Preprocess, strip out =#line= numbers and blank lines:
 cpp -DNCURSES_NOMACROS -DNCURSES_OPAQUE -D_XOPEN_SOURCE_EXTENDED -imacros stdio.h -imacros wchar.h $INPUT_FILE | egrep -v "^\s*$" | grep -v "^#" > stage1.h
 # Generate FFI signatures:
