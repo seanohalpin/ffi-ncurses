7787ccaa6bd3de828e131d374a6141abd7358a6f
Sean O'Halpin <sean.ohalpin@bbc.co.uk>
2011-01-05 16:53:18 +0000

    updated function signature generator scripts

--- tools/gensigs.sh
+++ tools/gensigs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#echo $0
+#echo $(dirname $0)
+BASEDIR=$(dirname $0)
+cd $BASEDIR
 INPUT_FILE=/usr/include/ncursesw/ncurses.h
 # INPUT_FILE=ncurses.h
 # INPUT_FILE=/usr/include/ncurses.h
 # Preprocess, strip out =#line= numbers and blank lines:
 cpp -DNCURSES_NOMACROS -DNCURSES_OPAQUE -D_XOPEN_SOURCE_EXTENDED -imacros stdio.h -imacros wchar.h $INPUT_FILE | egrep -v "^\s*$" | grep -v "^#" > stage1.h
 # Generate FFI signatures:
-ruby gentypes.rb $* stage1.h > cpp-gen.rb 2>cpp-unmapped.rb
+ruby gentypes.rb $* stage1.h > functions.rb 2>unmapped-functions.rb
+cp functions.rb ../lib/ffi-ncurses/functions.rb
 
