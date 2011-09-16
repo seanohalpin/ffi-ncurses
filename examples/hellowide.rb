9aed79800565f9ba7669726533c160047f9d1ff8
Sean O'Halpin <sean.ohalpin@gmail.com>
2011-09-16 12:37:57 +0100

    moved widechar stuff into examples/hellowide.rb

---
+++ examples/hellowide.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# -*- mode: ruby; coding: utf-8 -*-
+#
+# Sean O'Halpin, 2009-02-15
+#
+require 'ffi-ncurses'
+require 'ffi-ncurses/widechars'
+
+def centre(text)
+  col = (COLS() - WideChars.display_width(text.strip))/2
+  y, x = getyx(stdscr)
+  mvaddstr y, col, text
+end
+
+def fullwidth(txt)
+  txt.encode("UTF-32BE").codepoints.map{ |x|
+    case x
+    when 0x30..0x7F
+      x + 0xFEE0
+    else
+      x
+    end
+  }.pack("U*")
+end
+
+include FFI::NCurses
+begin
+  greeting = ARGV.shift || "World"
+  stdscr = initscr
+  raw
+  keypad stdscr, true
+  noecho
+  curs_set 0
+  clear
+  move (LINES() - 3)/3, 0
+  centre "Hello " + greeting + "\n"
+  centre "你好\n"
+  centre "ＨＥＬＬＯ ＷＯＲＬＤ\n"
+  centre fullwidth("[Testing 1234]\n\n")
+  centre "Press any key to continue"
+  refresh
+  ch = getch
+  flushinp
+  addstr sprintf("\nYou pressed %c (%d)", ch, ch)
+  refresh
+  sleep 1
+ensure
+  endwin
+end
