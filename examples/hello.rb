8b94cbbb76eac9b4a4db921b8f2c765cb86a211a
Sean O'Halpin <sean.ohalpin@gmail.com>
2011-09-16 12:41:02 +0100

    simplified examples/hello.rb - wide char stuff in examples/hellowide.rb

--- examples/hello.rb
+++ examples/hello.rb
@@ -7,9 +7,20 @@ require 'ffi-ncurses'
 require 'ffi-ncurses/widechars'
 
 def centre(text)
-  col = (COLS() - WideChars.display_width(text))/2
+  col = (COLS() - text.strip.size)/2
   y, x = getyx(stdscr)
   mvaddstr y, col, text
+end
+
+def fullwidth(txt)
+  txt.codepoints.map{ |x|
+    case x
+    when 0x30..0x7F
+      x + 0xFEE0
+    else
+      x
+    end
+  }.pack("U*")
 end
 
 include FFI::NCurses
@@ -20,24 +31,13 @@ begin
   keypad stdscr, true
   noecho
   curs_set 0
-  #  printw "Hello %s", :string, greeting # doesn't work in rbx
-  # addstr "Hello " + greeting + "\n"
-  # addstr "你好" + "\n"
-  # addstr "ＨＥＬＬＯ ＷＯＲＬＤ\n"
-  # addstr "-|" * 11 + "\n"
-
+  clear
   move (LINES() - 3)/3, 0
-  centre "Hello " + greeting
-  addstr "\n"
-  centre "你好"
-  addstr "\n"
-  centre "ＨＥＬＬＯ ＷＯＲＬＤ"
-  addstr "\n\n"
-  centre "Press any key to continue"
+  centre "Hello " + greeting + "\n\n"
+  centre "Press any key to continue\n"
   refresh
   ch = getch
   flushinp
-  #printw "\nYou pressed %c (%d)", :char, ch, :int, ch
   addstr sprintf("\nYou pressed %c (%d)", ch, ch)
   refresh
   sleep 1
